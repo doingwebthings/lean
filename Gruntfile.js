80ab5975e233ae87450cb8a3ac759ab83b842f53
Rolf Meyer <rosmeyer@gmail.com>
2014-06-26 16:42:52 +0200

    fiddled with grunt file.js and some libs

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,49 +1,63 @@
 'use strict';
 module.exports = function(grunt){
+    require('load-grunt-tasks')(grunt);
+
     grunt.initConfig({
-        //image optimization
-        imagemin: {
-            options: {
-                cache: false
-            },
-            dist: {
+
+            //image optimization
+            imagemin: {
                 options: {
-                    optimizationLevel: 5,
-                    progressive: true
+                    cache: false
                 },
-                files: [
-                    {
-                        expand: true,
-                        cwd: 'assets/img/',
-                        src: ['**/*.{PNG,JPG,JPEG,GIF,png,jpg,jpeg,gif}'],
-                        dest: 'assets/img/'
-                    }
-                ]
-            }
-        },
-        //LESS
-        less: {
-            dist: {
-                files: {
-                    'assets/css/styles.css': [
-                        'assets/less/styles.less'
+                dist: {
+                    options: {
+                        optimizationLevel: 5,
+                        progressive: true
+                    },
+                    files: [
+                        {
+                            expand: true,
+                            cwd: 'assets/img/',
+                            src: ['**/*.{PNG,JPG,JPEG,GIF,png,jpg,jpeg,gif}'],
+                            dest: 'assets/img/'
+                        }
                     ]
-                },
-                options: {
-                    compress: false,
-                    // LESS source map
-                    // To enable, set sourceMap to true and update sourceMapRootpath based on your install
-                    sourceMap: true,
-                    sourceMapFilename: 'assets/css/main.min.css.map'
-//                    sourceMapRootpath: '/app/themes/lean/'
                 }
-            }
-        },
-        //uglify and concat js files
-        uglify: {
-            dist: {
-                files: {
-                    'assets/js/scripts.min.js': [
+            },
+
+            notify: {
+                default: {
+                    options: {
+                        title: 'Task done',
+                        message: 'Now keep on coding'
+                    }
+                }
+            },
+
+            //LESS
+            less: {
+                dist: {
+                    files: {
+                        'assets/css/styles.css': [
+                            'assets/less/styles.less'
+                        ]
+                    },
+                    options: {
+                        compress: false,
+                        // LESS source map
+                        // To enable, set sourceMap to true and update sourceMapRootpath based on your install
+                        sourceMap: true,
+                        sourceMapFilename: 'assets/css/main.min.css.map'
+//                    sourceMapRootpath: '/app/themes/lean/'
+                    }
+                }
+            },
+
+            //uglify and concat js files
+            uglify: {
+                dist: {
+                    files: {
+                        'assets/js/scripts.min.js': [
 //                        'assets/bower_components/bootstrap/js/transition.js',
 //                        'assets/bower_components/bootstrap/js/alert.js',
 //                        'assets/bower_components/bootstrap/js/button.js',
@@ -56,66 +70,65 @@ module.exports = function(grunt){
 //                        'assets/bower_components/bootstrap/js/scrollspy.js',
 //                        'assets/bower_components/bootstrap/js/tab.js',
 //                        'assets/bower_components/bootstrap/js/affix.js',
-                        'assets/js/scripts.js'
-                    ]
-                },
-                options: {
-                    sourceMap: 'assets/js/scripts.min.js.map',
-                    compress: {
-                        drop_console: true
+                            'assets/js/scripts.js'
+                        ]
+                    },
+                    options: {
+                        sourceMap: 'assets/js/scripts.min.js.map',
+                        compress: {
+                            drop_console: true
+                        }
                     }
                 }
-            }
-        },
-        watch: {
-            less: {
-                files: [
-                    'assets/less/*.less'
-                ],
-                tasks: ['less']
             },
-            js: {
-                files: [
-                    'assets/js/scripts.js'
-                ],
-                tasks: [ 'uglify']
-            },
-            livereload: {
-                // Browser live reloading
-                // https://github.com/gruntjs/grunt-contrib-watch#live-reloading
-                options: {
-                    livereload: true
+
+            watch: {
+                less: {
+                    files: [
+                        'assets/less/*.less'
+                    ],
+                    tasks: ['less', 'notify']
                 },
-                files: [
+                js: {
+                    files: [
+                        'assets/js/scripts.js'
+                    ],
+                    tasks: [ 'uglify', 'notify']
+                },
+                livereload: {
+                    // Browser live reloading
+                    // https://github.com/gruntjs/grunt-contrib-watch#live-reloading
+                    options: {
+                        livereload: true
+                    },
+                    files: [
+                        'assets/css/styles.css',
+                        'assets/js/scripts.js'
+                    ]
+                }
+            },
+            clean: {
+                dist: [
                     'assets/css/styles.css',
-                    'assets/js/scripts.js'
+                    'assets/js/scripts.min.js'
                 ]
             }
-        },
-        clean: {
-            dist: [
-                'assets/css/styles.css',
-                'assets/js/scripts.min.js'
-            ]
         }
-    });
+    );
 
-    // Load tasks
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
+
 
     // Register tasks
     grunt.registerTask('default', [
         'clean',
-        'less',
-        'uglify',
-        'imagemin'
+        'newer:less',
+        'newer:uglify',
+        'imagemin',
+        'notify'
     ]);
     grunt.registerTask('dev', [
         'watch'
     ]);
 
-};
+}
+;
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,4 @@
 /**
  * Created by rolfmeyer on 26.06.14.
  */
-alert('rolfix');
+alert('rolf1c xy asdfkkk');

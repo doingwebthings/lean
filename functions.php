2622b68dbf137c26678913fb4ea6745a20f44654
Rolf Meyer <rosmeyer@gmail.com>
2015-03-30 16:58:11 +0200

    added composer to theme

--- functions.php
+++ functions.php
@@ -1,44 +1,50 @@
 <?php
 /**
+ * load composer autoloader
+ */
+require( get_template_directory() . '/vendor/autoload.php' );
+
+
+/**
  * changes to the WordPress admin screen
  */
-include(get_template_directory() . '/includes/admin-setup.php');
+include( get_template_directory() . '/includes/admin-setup.php' );
 
 
 /**
  * theme-setup
  */
-include(get_template_directory() . '/includes/theme-setup.php');
-include(get_template_directory() . '/includes/theme-setup-bootstrap.php');
+include( get_template_directory() . '/includes/theme-setup.php' );
+include( get_template_directory() . '/includes/theme-setup-bootstrap.php' );
 
 
 /**
  * change mail settings
  */
-include(get_template_directory() . '/includes/mail-setup.php');
+include( get_template_directory() . '/includes/mail-setup.php' );
 
 
 /**
  * add custom template tags / conditionals
  */
-include(get_template_directory() . '/includes/template-tags.php');
+include( get_template_directory() . '/includes/template-tags.php' );
 
 
 /**
  * generic shortcodes go here
  */
-include(get_template_directory() . '/includes/shortcodes.php');
+include( get_template_directory() . '/includes/shortcodes.php' );
 
 
 /**
  * load post-types
  */
-include(get_template_directory() . '/includes/post-types.php');
+include( get_template_directory() . '/includes/post-types.php' );
 
 
 /**
  * set up ajax calls here
  */
-include(get_template_directory() . '/includes/ajax.php');
+include( get_template_directory() . '/includes/ajax.php' );
 
 
--- includes/post-types.php
+++ includes/post-types.php
@@ -2,4 +2,7 @@
 /**
  * include all custom-post-types from folder "post-types" here
  */
-include(get_template_directory() . '/post-types/hund.php');
+if ( file_exists( get_template_directory() . '/post-types/hund.php' ) )
+{
+	include( get_template_directory() . '/post-types/hund.php' );
+}

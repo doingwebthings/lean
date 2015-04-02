4bba552eb2eeb7003057ae219d943c0f83b0e5fd
Rolf Meyer <rosmeyer@gmail.com>
2015-04-02 10:41:23 +0200

    livereload in dev only, cleaned up footer

--- partials/footer.php
+++ partials/footer.php
@@ -1,24 +1,3 @@
-<div class="container-fluid footer">
-
-    <div class="container">
-        <footer>
-            <div class="row row-gutter">
-                <div class="col-md-6">
-                    <div class="gutter-test">
-                        <div class="footer-widgets">
-                            <?php get_template_part('partials/widgets-secondary'); ?>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="gutter-test">y</div>
-                </div>
-            </div>
-        </footer>
-    </div>
-</div>
-
 <?php wp_footer(); ?>
-
 </body>
 </html>
--- partials/header.php
+++ partials/header.php
@@ -1,19 +1,25 @@
 <!DOCTYPE html>
 <html class="no-js" <?php language_attributes(); ?>>
-<head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0">
+	<head>
+		<meta charset="<?php bloginfo('charset'); ?>">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0">
 
-    <title><?php wp_title('|', true, 'right'); ?></title>
+		<title><?php wp_title('|', true, 'right'); ?></title>
 
-    <link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url(); ?>img/favicon.ico">
+		<link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url(); ?>img/favicon.ico">
 
-    <?php wp_head(); ?>
+		<?php wp_head(); ?>
 
-</head>
+		<?php if(is_development() === true): ?>
+			<script type="text/javascript" src="<?php echo base_url(); ?>livereload.js"></script>
+		<?php endif; ?>
 
-<body <?php body_class() ?>>
+		<link type="text/css" rel="stylesheet" href="http://fast.fonts.net/cssapi/dc663f5b-95e5-4420-8b5e-cb6ce1e6a24f.css">
 
-<?php get_template_part('partials/menu-primary'); ?>
+	</head>
+
+	<body <?php body_class() ?>>
+
+
 
